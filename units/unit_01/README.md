b18a5c514d0248a4e93d127f322eeec92357a268
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2025-01-01 14:16:53 +0100

    Refacto unit 1 to easily try other envs.

--- units/unit_01/README.md
+++ units/unit_01/README.md
@@ -1,14 +1,32 @@
 # Unit 1: Train your first Deep Reinforcement Learning Agent
 
-**Dependencies:**
+## Training
+
+You can run the training of the ppo with this command:
+
+```bash
+python src/train.py -e "LunarLander-v3"
+```
+
+Edit the src/train.py file to try different parameters. 
+
+## Dependencies
 
 ```bash
 pip install torch gymnasium[box2d] gymnasium[other] stable_baselines3 tensorboard huggingface-hub huggingface-sb3
 ```
 
-**Project Structure:**
+## Project Structure
 
-* **train_lunar_lander.py:** Trains a PPO agent on the Lunar Lander environment.
+* **train.py:** Trains a PPO agent on the Lunar Lander environment.
 * **test_lunar_lander.py:** Evaluates the trained agent and logs the performance.
 * **watch_lunar_lander.py:** Visualizes the trained agent interacting with the environment.
-* **push_to_hf.py:** Pushes the trained model to the Hugging Face Hub.
+* **hugface.py:** Pushes the trained model to the Hugging Face Hub.
+
+## Tensorboard:
+
+To monitor trainning progress:
+
+```bash
+tensorboard --logdir ./logs
+```
---
+++ units/unit_01/src/train.py
@@ -0,0 +1,54 @@
+import gymnasium as gym
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.monitor import Monitor
+
+from argparse import ArgumentParser
+
+# Parse Args, username and token
+parser = ArgumentParser()
+
+parser.add_argument("-e", "--env", dest="env_id",
+                    help="push to the huggingface user's repo")
+
+# Main
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    env_id = args.env_id
+
+    nb_steps = 1_500_000
+
+    eval_freq = 10_000
+    n_eval_episodes = 200
+
+    # 8 give faster results (~20 min on cpu) but less stable
+    # 16 takes longer (~3 hours on cpu) but have better generalisation and stability
+    n_training_envs = 16
+
+    model_save_path = "./models/ppo-" + env_id
+
+    env = make_vec_env(env_id, n_envs=n_training_envs)
+    model = PPO(
+        policy="MlpPolicy",
+        env=env,
+        learning_rate=3e-3,
+        n_steps=512,
+        batch_size=32,
+        n_epochs=3,
+        gamma=0.999,
+        gae_lambda=0.98,
+        ent_coef=0.01,
+        verbose=0,
+        tensorboard_log="./logs/ppo_" + env_id + "/"
+    )
+
+    eval_env = Monitor(gym.make(env_id))
+    eval_callback = EvalCallback(eval_env, best_model_save_path=model_save_path,
+                              log_path=model_save_path, eval_freq=max(eval_freq // n_training_envs, 1),
+                              n_eval_episodes=n_eval_episodes, deterministic=True, render=False)
+
+    model.learn(total_timesteps=nb_steps, callback=eval_callback)
