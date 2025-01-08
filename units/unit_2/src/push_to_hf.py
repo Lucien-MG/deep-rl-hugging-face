4c71eb0b47e8536dd507f43ac8ee1bd687f1f323
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2025-01-08 20:51:58 +0100

    Refacto, rename push to hugging face script..

--- units/unit_2/src/push_to_hf.py
+++ units/unit_2/src/push_to_hf.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 
     env = gym.make(args.env_id, render_mode="rgb_array")
 
-    n_training_episodes = 150_000
+    n_training_episodes = 100_000
     max_steps = 100
     learning_rate = 0.1
     n_eval_episodes = 100
@@ -31,6 +31,6 @@ if __name__ == '__main__':
     # Pickle the model
     model_path = "./models/q-learning-" + args.env_id  + ".pkl"
     with open(model_path, "rb") as f:
-        model = pickle.load(f)
+        q_table = pickle.load(f)
 
-    push_model(args.username, args.token, "qlearning", agent.q_table, args.env_id, n_training_episodes, n_eval_episodes, eval_seed, learning_rate)
+    push_model(args.username, args.token, "qlearning", q_table, args.env_id, n_training_episodes, n_eval_episodes, eval_seed, learning_rate)
