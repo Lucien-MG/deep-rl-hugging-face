774761c3d7899ac808ce689a6d9fa0b7489c356e
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2025-01-10 22:12:55 +0100

    Refacto dependencies script.

--- units/unit_1_bonus/dependencies.sh
+++ units/unit_1_bonus/dependencies.sh
@@ -2,13 +2,11 @@
 
 # Download Huggy env:
 wget "https://github.com/huggingface/Huggy/raw/main/Huggy.zip" -O ./Huggy.zip
-
 unzip -d ./envs/ ./Huggy.zip
-
 chmod -R 755 ./envs/Huggy/Huggy.x86_64
-
 rm ./Huggy.zip
 
 # Download unity's ml agents
 # Clone the repository (can take 3min)
+# It might requires to modify some dependencies for this to work
 git clone --depth 1 https://github.com/Unity-Technologies/ml-agents
