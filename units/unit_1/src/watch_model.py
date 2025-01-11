00806a16d998d231235cd5370fce703e9766df9f
Lucien Martin-Gaffé <lucien.martingaffe@gmail.com>
2025-01-11 20:50:30 +0100

    Can now watch model from local file or directly from hf hub.

--- units/unit_1/src/watch_model.py
+++ units/unit_1/src/watch_model.py
@@ -1,26 +1,38 @@
+import numpy
 import gymnasium as gym
 
 from stable_baselines3 import PPO
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.monitor import Monitor
+
+from huggingface_sb3 import load_from_hub
 
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
 
 parser.add_argument("-e", "--env", dest="env_id",
-                    help="push to the huggingface user's repo")
+                    help="Choose the environment to run")
+
+parser.add_argument("-r", "--repo-id", dest="repo_id",
+                    help="Choose from which repo pull the model")
+
+parser.add_argument("-f", "--filename", dest="filename",
+                    help="Choose the model filename (if no repo id specifided look for local file))")
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    env_name = args.env_id
-    model_name = "./models/ppo-" + env_name + "/best_model"
+    if args.repo_id != None:
+        print("Loading model from hub...")
+        model_filename = load_from_hub(args.repo_id, args.filename)
+    else:
+        print("Loading model from local...")
+        model_filename = args.filename
 
-    env = gym.make(env_name, render_mode="human")
-    model = PPO.load(model_name)
+    model = PPO.load(model_filename)
 
+    print(model)
+
+    env = gym.make(args.env_id, render_mode="human")
     obs, info = env.reset()
 
     done = False
